6bd1194830d8a54d7ec94d140d1c79b14315a08b
arshitrupanii <arshitrupani6@gmail.com>
2025-02-16 16:24:38 +0530

    x

---
+++ convex/schema.ts
@@ -0,0 +1,16 @@
+import { table } from "console";
+import { defineSchema, defineTable } from "convex/server";
+import {v} from "convex/values";
+
+
+export default defineSchema({
+
+    user : defineTable({
+        name : v.string(),
+        email : v.string(),
+        image : v.optional(v.string()),
+        role : v.union(v.literal("candidate"), v.literal("interviwer")),
+        clerkId : v.string()
+
+    })
+});
